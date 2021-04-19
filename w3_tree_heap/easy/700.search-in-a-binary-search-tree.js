6a5617a3e86c3f89ae4ac18da3ae2ab7cdb389a5
Tiffany Jung <tiffanyrong20@gmail.com>
2021-04-19 18:25:57 +0800

    tree

--- w3_tree_heap/easy/700.search-in-a-binary-search-tree.js
+++ w3_tree_heap/easy/700.search-in-a-binary-search-tree.js
@@ -19,7 +19,11 @@
  * @return {TreeNode}
  */
 var searchBST = function(root, val) {
-    
+    if(!root) return null;
+    if(root.val === val) return root;
+    if(val < root.val) return searchBST(root.left,val);
+    if(val > root.val) return searchBST(root.right,val);
+
 };
 // @lc code=end
 
