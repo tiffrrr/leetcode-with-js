0ce6f6f898b2a9a34daf3126f528ab298ded5a07
TiffanyRong <tiffanyrong20@gmail.com>
2022-04-25 22:29:33 +0800

    finish 111

--- w4_graph_hash_BFS_DFS/easy/111.minimum-depth-of-binary-tree.js
+++ w4_graph_hash_BFS_DFS/easy/111.minimum-depth-of-binary-tree.js
@@ -18,7 +18,23 @@
  * @return {number}
  */
 var minDepth = function(root) {
-    
+    if(!root) return 0 ;
+    let depth = 1;
+    let queue = [root];
+    if(!root.left && !root.right) return depth;
+
+    while(queue.length > 0){
+
+        let queueLength = queue.length;
+        for(let i=0;i<queueLength;i++){
+            let node = queue.shift();
+            if(!node.left && !node.right) return depth;
+            else{
+                if(node.left) queue.push(node.left);
+                if(node.right) queue.push(node.right);
+            }
+        }
+        depth++;
+    }
 };
 // @lc code=end
-
