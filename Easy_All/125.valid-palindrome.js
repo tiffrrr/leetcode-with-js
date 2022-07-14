83928c5873becc9ccb8a3a835521159bea81504c
TiffanyRong <tiffanyrong20@gmail.com>
2022-07-14 23:37:50 +0800

    feat: finish 125, 141

--- Easy_All/125.valid-palindrome.js
+++ Easy_All/125.valid-palindrome.js
@@ -9,8 +9,44 @@
  * @param {string} s
  * @return {boolean}
  */
-var isPalindrome = function(s) {
-    
-};
-// @lc code=end
+// 0 7
+// 1 6
+// 2 5
+// 3 4
 
+// 0 6
+// 1 5
+// 2 4
+// 3
+var isPalindrome = function (s) {
+    if (s.length <= 1) { return true; }
+    const arr = s.toLowerCase().split('').filter(e => e.match(/^[a-z0-9]/));
+    // 如果全是特殊符號的話，清完會等於[]，不排除的話後面會出錯。
+    if (arr.length < 2) return true;
+    let start = 0;
+    let end = arr.length - 1;
+    let isSame = false;
+    while (start < end) {
+        if (arr[start] == arr[end]) {
+            start += 1;
+            end -= 1;
+            isSame = true;
+            console.log('same', start)
+        } else {
+            console.log('not same', end)
+            isSame = false;
+            break;
+        }
+    }
+    return isSame;
+
+};
+// const string = "A man, a plan, a canal: Panama";
+// const string = "rac e a car";
+// const string = ".,";
+// const string = "0p";
+// console.log(isPalindrome(string))
+// @lc code=end
+// 錯誤的地方
+// 忘記考慮全是特殊符號的狀況ex:".,"
+// 題目沒讀懂。non-alphanumeric characters = 英文+數字ex:“0p”
--- Easy_All/141.linked-list-cycle.js
+++ Easy_All/141.linked-list-cycle.js
@@ -19,19 +19,79 @@
  */
 
 // 快慢指標
-var hasCycle = function(head) {
+var hasCycle = function (head) {
     let fast = head;
     let slow = head;
     // 停止條件1:出現null，表示不是環狀，return false
-    while(fast && fast.next && fast.next.next){
+    while (fast && fast.next && fast.next.next) {
         fast = fast.next.next;
         slow = slow.next;
         // 停止條件2: 快慢指標相遇，return true，while迴圈停止
-        if(fast === slow){
+        if (fast === slow) {
             return true;
         }
     }
     return false;
 };
-// @lc code=end
 
+
+// // 寫第二次：第一版
+// 只有一個node 也會分 [1]有環 [1]無環
+
+// var hasCycle = function (head) {
+//     // 其實不需要額外判斷，因為如果null的話後面會直接回傳false
+//     if (!head) { return false };
+//     let slow = head;
+//     let fast = head;
+//     // 條件寫錯，無窮回圈。這裡要寫的是「執行的條件」，不是「停止的條件」
+//     while (slow == fast) {
+//         // 不用判斷slow,因為fast走過，所以slow一定都有。
+//         //  反而要判斷fast + fast.next +fast.next.next。不然如果後面是null的話會出錯。
+//         if (fast && slow) {
+//             slow = slow.next;
+//             fast = fast.next.next;
+//         } else {
+//             return false;
+//         }
+//     }
+//     return true;
+// };
+
+
+// // 寫第二次：第二版
+// var hasCycle = function (head) {
+//     let slow = head;
+//     let fast = head;
+//     // 因為slow = fast = head,不滿足while的執行條件
+//     // 所以根本不會進到while。永遠回傳true
+//     while (slow !== fast) {
+//         if (fast && fast.next && fast.next.next) {
+//             slow = slow.next;
+//             fast = fast.next.next;
+//         } else {
+//             return false;
+//         }
+//     }
+//     return true;
+// };
+
+
+// // 寫第二次：第三版
+// var hasCycle = function (head) {
+//     if (!head || !head.next || !head.next.next) { return false };
+//     let slow = head.next;
+//     let fast = head.next.next;
+
+//     while (slow !== fast) {
+//         if (fast && fast.next && fast.next.next) {
+//             slow = slow.next;
+//             fast = fast.next.next;
+//         } else {
+//             return false;
+//         }
+//     }
+//     return true;
+// };
+
+
+// @lc code=end
