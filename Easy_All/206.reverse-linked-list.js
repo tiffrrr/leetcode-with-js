740c3cde99cde566027b2e8afa9f793408ac578c
TiffanyRong <tiffanyrong20@gmail.com>
2022-07-22 22:44:26 +0800

    feat: finish 206 217 on 2022/7/15

--- Easy_All/206.reverse-linked-list.js
+++ Easy_All/206.reverse-linked-list.js
@@ -17,24 +17,56 @@
  * @return {ListNode}
  */
 
-var reverseList = function(head) {
-    if(!head){
-        return null
-    }
-    if(!head.next){
-        return head;
-    }
-    let currNode = head;
-    let prevNode = null;
-    let nextNode;
-    while(nextNode !== null){
-        nextNode = currNode.next
-        currNode.next = prevNode
-        prevNode = currNode
-        currNode = nextNode
-    }
-    return prevNode
-
+var reverseList = function (head) {
+    if (!head) return head;
+    if (!head.next) return head;
+    let last = reverseList(head.next);
+    head.next.next = head;
+    head.next = null;
+    return last
 };
+// (1 -> 2 -> 3)
+// last = reverse(2)
+//         last = reverse(3)
+//         2 -> 3 -> null
+//             return 3 -> null
+//         2.next.next = 2  (2 <-> 3)
+//         2.next = null  (null <- 2 <- 3)
+//         return  (null <- 2 <- 3)
+// // (1 -> 2 <- 3且有另一條 2 -> null   )
+// 1.next.next = 1  (1 <-> 2 <- 3)
+// 1.next= null( null <-1 <- 2 <- 3)
+// return ( null <-1 <- 2 <- 3)
+
+
 // @lc code=end
 
+// 錯誤思考：在想怎麼把點交換位置，但應該要思考中間的箭頭怎麼走。
+// 箭頭也是可以先拔掉的喔！之後再連回來
+// 圈圈不動，線動，而不是線不動，圈圈動。
+// 不是array，有固定排序只能交換位置。
+
+
+// var reverseList = function(head) {
+//     if(!head){
+//         return null
+//     }
+//     if(!head.next){
+//         return head;
+//     }
+//     let currNode = head;
+//     let prevNode = null;
+//     let nextNode;
+//     while(nextNode !== null){
+//         nextNode = currNode.next
+//         currNode.next = prevNode
+//         prevNode = currNode
+//         currNode = nextNode
+//     }
+//     return prevNode
+
+// };
+// 1 -> 2 -> 3 -> null
+// null <- 1   2 -> 3
+// null <- 1 <-2    3
+// null <- 1 <-2 <- 3
--- Easy_All/217.contains-duplicate.js
+++ Easy_All/217.contains-duplicate.js
@@ -9,8 +9,16 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var containsDuplicate = function(nums) {
-    
+var containsDuplicate = function (nums) {
+    var arr = [];
+    for (var i = 0; i < nums.length; i++) {
+        if (arr.indexOf(nums[i]) > -1) {
+            return true
+        } else {
+            arr.push(nums[i]);
+        }
+    }
+    return false;
 };
 // @lc code=end
 
